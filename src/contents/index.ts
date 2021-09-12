a32783f1e4dcfb6539f18882953e326f275f0e45
Rafael Sandoval <sandovalrd@gmail.com>
2021-09-12 20:11:47 -0300

    rename deployment project

--- src/contents/index.ts
+++ src/contents/index.ts
@@ -237,7 +237,7 @@ export const CV = {
 export const PROJECTS_EN = [
   {
     description: "Web portfolio available to show the profile and projects of the developer.",
-    deployment: "porfolio-cyan.vercel.app",
+    deployment: "porfolio-sandovalrd.vercel.app",
     domain: "https://sandovalrd.website/",
     state: "In posgress",
     color: "bg-yellow-500",
@@ -249,7 +249,7 @@ export const PROJECTS_EN = [
 export const PROJECTS_ES = [
   {
     description: "Portafolio web disponible para mostrar el perfil y proyectos del desarrollador.",
-    deployment: "porfolio-cyan.vercel.app",
+    deployment: "porfolio-sandovalrd.vercel.app",
     domain: "https://sandovalrd.website/",
     state: "En progreso",
     color: "bg-yellow-500",
